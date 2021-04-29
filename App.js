c962d562b61108d4d56ee506ed90720efcd27f00
Yan Sena <contato@yandev.com.br>
2021-04-28 21:04:47 -0300

    Primeiro Component

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppMain } from './src/pages/AppMain';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppMain/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/AppMain.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+export function AppMain() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Primeiro Componente</Text>
+      </View>
+    </SafeAreaView>
+  );
+} 
+
+
+  
